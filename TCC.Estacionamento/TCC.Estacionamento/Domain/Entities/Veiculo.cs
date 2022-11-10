21f1330988954df81b01f41c526661ad65b8bf2b
Glauber Brennon <glauberbrennon@gmail.com>
2022-11-10 17:56:40 -0300

    stranger things from .net files

--- TCC.Estacionamento/TCC.Estacionamento/Domain/Entities/Veiculo.cs
+++ TCC.Estacionamento/TCC.Estacionamento/Domain/Entities/Veiculo.cs
@@ -18,45 +18,6 @@ namespace TCC.Estacionamento.Domain.Entities
         public DateTime HoraEntrada { get; set; }
         public DateTime HoraSaida { get; set; }
 
-
-        public Placa Placa
-        {
-            get
-            {
-                return _placa;
-            }
-            set
-            {
-                // Checa se o valor possui pelo menos 8 caracteres
-                if (value.PlacaVeiculo.Length != 8)
-                {
-                    throw new FormatException(" A placa deve possuir 8 caracteres");
-                }
-                for (int i = 0; i < 3; i++)
-                {
-                    //checa se os 3 primeiros caracteres são numeros
-                    if (char.IsDigit(value.PlacaVeiculo[i]))
-                    {
-                        throw new FormatException("Os 3 primeiros caracteres devem ser letras!");
-                    }
-                }
-                //checa o Hifem
-                if (value.PlacaVeiculo[3] != '-')
-                {
-                    throw new FormatException("O 4° caractere deve ser um hífen");
-                }
-                //checa se os 3 primeiros caracteres são numeros
-                for (int i = 4; i < 8; i++)
-                {
-                    if (!char.IsDigit(value.PlacaVeiculo[i]))
-                    {
-                        throw new FormatException("Do 5º ao 8º caractere deve-se ter um número!");
-                    }
-                }
-                _placa = value;
-
-            }
-        }
         public Proprietario Proprietario
         {
             get
--- TCC.Estacionamento/TCC.Estacionamento/Domain/ValueObjects/Placa.cs
+++ TCC.Estacionamento/TCC.Estacionamento/Domain/ValueObjects/Placa.cs
@@ -1,13 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TCC.Estacionamento.Domain.ValueObjects
 {
     public class Placa
     {
-        public string PlacaVeiculo;
+        private string _value { get; }
+
+        private Placa(string placaValidValue) {
+            _value = placaValidValue;
+        }
+
+        public static Placa Create(string rawPlaca){
+            ValidatePossuiOitoCaracteres(rawPlaca);
+            ValidatePrimeirosTresPrimeirosCaracteresSaoLetras(rawPlaca);
+
+            //checa o Hifem
+            if (rawPlaca[3] != '-')
+            {
+                throw new FormatException("O 4° caractere deve ser um hífen");
+            }
+            //checa se os 3 primeiros caracteres são numeros
+            for (int i = 4; i < 8; i++)
+            {
+                if (!char.IsDigit(rawPlaca[i]))
+                {
+                    throw new FormatException("Do 5º ao 8º caractere deve-se ter um número!");
+                }
+            }
+
+            return new Placa(rawPlaca);
+        }
+
+        private static void ValidatePossuiOitoCaracteres(string rawPlaca) {
+            // Checa se o valor possui pelo menos 8 caracteres
+            if (rawPlaca.Length != 8)
+            {
+                throw new FormatException(" A placa deve possuir 8 caracteres");
+            }
+        }
+
+        
+        private static void ValidatePrimeirosTresPrimeirosCaracteresSaoLetras(string rawPlaca) {
+            for (int i = 0; i < 3; i++)
+            {
+                //checa se os 3 primeiros caracteres são numeros
+                if (char.IsDigit(rawPlaca[i]))
+                {
+                    throw new FormatException("Os 3 primeiros caracteres devem ser letras!");
+                }
+            }
+        }
     }
 }
