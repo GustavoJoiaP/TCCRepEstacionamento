f01313b0ac1bbd23f25d5c64b9ae29b055f2b456
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2022-11-12 08:55:59 -0300

    criação do dto de entrada e saida para inserção de um veiculo no estacionamento. criação do metodo de inserção de veiculo no repository

--- TCC.Estacionamento/Domain/Repositories/VeiculoRepository.cs
+++ TCC.Estacionamento/Domain/Repositories/VeiculoRepository.cs
@@ -7,11 +7,17 @@ namespace TCC.Estacionamento.Domain.Repositories
     public interface VeiculoRepository
     {
         public Veiculo ProcurarVeiculoPorPlaca(Placa placa);
+        public Veiculo AdicionarVeiculoNoPatio(Placa placa);
 
     }
 
     public class MySqlVeiculoRepository : VeiculoRepository
     {
+        public Veiculo AdicionarVeiculoNoPatio(Placa placa)
+        {
+            throw new NotImplementedException();
+        }
+
         public Veiculo ProcurarVeiculoPorPlaca(Placa placa)
         {
             throw new NotImplementedException();
--- TCC.Estacionamento/Domain/Services/RegistrarEntradaVeiculoService.cs
+++ TCC.Estacionamento/Domain/Services/RegistrarEntradaVeiculoService.cs
@@ -3,16 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TCC.Estacionamento.Domain.DataTransferObjects;
 using TCC.Estacionamento.Domain.Entities;
+using TCC.Estacionamento.Domain.Repositories;
 
 namespace TCC.Estacionamento.Domain.Services
 {
     public class RegistrarEntradaVeiculoService
     {
-        public static void RegistrarEntradaVeiculo(Veiculo veiculo, Patio estacionamento)
+        private VeiculoRepository _veiculoRepository;
+        private Patio _patio;
+        public RegistrarEntradaVeiculoService(VeiculoRepository veiculoRepository, Patio patio)
         {
-            veiculo.HoraEntrada = DateTime.Now;
-            estacionamento.Veiculos.Add(veiculo);
+            _veiculoRepository = veiculoRepository;
+            _patio = patio;
         }
+
+        public ResultadoRegistrarEntradaVeiculoDTO RegistrarEntradaVeiculo(RegistrarEntradaVeiculoDTO registrarEntradaVeiculoDTO)
+        {
+            var resultadoRegistrarEntradaVeiculoDTO = new ResultadoRegistrarEntradaVeiculoDTO();
+            return registrarEntradaVeiculoDTO;
+        }
+
+        //public static void RegistrarEntradaVeiculo(Veiculo veiculo, Patio estacionamento)
+        //{
+        //    veiculo.HoraEntrada = DateTime.Now;
+        //    estacionamento.Veiculos.Add(veiculo);
+        //}
     }
 }
